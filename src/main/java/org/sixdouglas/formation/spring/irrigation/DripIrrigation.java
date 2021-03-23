009e3b25ce640964801b45ce4cbb9a34e82b6643
Tristan <mytic12@hotmail.fr>
2021-03-23 11:08:25 +0100

    push part 1

--- src/main/java/org/sixdouglas/formation/spring/irrigation/DripIrrigation.java
+++ src/main/java/org/sixdouglas/formation/spring/irrigation/DripIrrigation.java
@@ -5,12 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.time.*;
+
 @Component
 public class DripIrrigation {
     private static Logger LOGGER = LoggerFactory.getLogger(DripIrrigation.class);
 
     public Flux<Drop> followDrops() {
-        //TODO Create a Flux that would emit a Drop every 20 millis seconds
-        return null;
+        //TODO Create a Flux that would emit a Drop every 20 milliseconds
+        Flux<Drop> dropFlux = Flux
+                .interval(Duration.ofMillis(20))
+                .map(d-> Drop.builder().greenHouseId(1)
+                        .rowId(1).dropperId(1)
+                        .instant(Instant.now())
+                        .build());
+
+
+        return dropFlux;
     }
 }
